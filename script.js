cc821de124b4570632d32e6408b7c34f1cb2e2d9
MannyEngi <talk2cestoni@yahoo.com>
2023-10-13 18:43:11 -0500

    Class structrure for Book

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ class User {
     }
 }
 
+class Book{
+    constructor(title, author, year, genre, read = false) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.genre = genre;
+    this.read = read;   
+    }
+
+    markAsRead() {
+        this.read = true;
+        return this;
+    }
+}
+
+
+
